9404bb5684f96fb8657edf131262522887893f79
Akansha Pal <akanshapal1998@gmail.com>
2025-09-21 00:20:41 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical_chatbot",
+    version="o.1.0",
+    author="Akansha Pal",
+    author_email="akanshapal1998@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
